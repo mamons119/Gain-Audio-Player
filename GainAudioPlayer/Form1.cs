bcd59720d310b82430da35676bf42149c9de5320
Mamons99 <mamone119@gmail.com>
2026-06-06 11:46:09 +0200

    Primo tentativo di conversione in PCM

--- GainAudioPlayer/Form1.cs
+++ GainAudioPlayer/Form1.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio.Wave;
 
 namespace GainAudioPlayer
 {
@@ -15,6 +18,49 @@ namespace GainAudioPlayer
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        List<string> codaAudio = new List<string>();
+
+        private void caricaFileAudio(object sender, EventArgs e)
+        {
+            string audio = aggiungiAudioInCoda();
+            if (audio != null)
+            {
+                var reader = new AudioFileReader(audio);
+                float[] buffer = new float[4096];
+
+                int samplesRead;
+
+                while ((samplesRead = reader.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    for (int i = 0; i < samplesRead; i++)
+                    {
+                        float sample = buffer[i];
+
+                        Console.WriteLine(sample);
+                        Debug.WriteLine(sample);
+                    }
+                }
+            }
+        }
+
+        private string aggiungiAudioInCoda()
+        {
+            string filePath = "";
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "Audio File (*.mp3)|*.mp3";
+                dialog.Title = "Seleziona un file MP3";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = dialog.FileName;
+                }
+            }
+
+            return filePath;
         }
     }
 }
