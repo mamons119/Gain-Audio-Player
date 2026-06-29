e6e10266303b4e1d71dec1abbc0abd303efa3a06
Mamons99 <mamone119@gmail.com>
2026-06-29 16:10:19 +0200

    Implementato il grafico per la forma d'onda della canzone

--- GainAudioPlayer/Form1.Designer.cs
+++ GainAudioPlayer/Form1.Designer.cs
@@ -29,39 +29,80 @@
         private void InitializeComponent()
         {
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.caricaAudioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel_nomeCanzone = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // button1
+            // menuStrip1
             // 
-            this.button1.Location = new System.Drawing.Point(79, 43);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(120, 35);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Carica Audio";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.caricaFileAudio);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.caricaAudioToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1272, 30);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // caricaAudioToolStripMenuItem
+            // 
+            this.caricaAudioToolStripMenuItem.Name = "caricaAudioToolStripMenuItem";
+            this.caricaAudioToolStripMenuItem.Size = new System.Drawing.Size(108, 26);
+            this.caricaAudioToolStripMenuItem.Text = "Carica Audio";
+            this.caricaAudioToolStripMenuItem.Click += new System.EventHandler(this.caricaFileAudio);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel_nomeCanzone});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 550);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1272, 26);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel_nomeCanzone
+            // 
+            this.toolStripStatusLabel_nomeCanzone.Name = "toolStripStatusLabel_nomeCanzone";
+            this.toolStripStatusLabel_nomeCanzone.Size = new System.Drawing.Size(195, 20);
+            this.toolStripStatusLabel_nomeCanzone.Text = "Nessun file mp3 selezionato";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1272, 576);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem caricaAudioToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_nomeCanzone;
     }
 }
 
--- GainAudioPlayer/Form1.cs
+++ GainAudioPlayer/Form1.cs
@@ -15,35 +15,108 @@ namespace GainAudioPlayer
 {
     public partial class Form1 : Form
     {
+
+        Bitmap canvas;
+        Graphics g;
+
+        int width_osc = 1290;
+        int height_osc = 623;
+        int centerY = 0;
+        int lastY;
+
+        Pen utilityPen = Pens.Gray;
+        Pen samplePen = Pens.Green;
+
         public Form1()
         {
+            canvas = new Bitmap(width_osc, height_osc);
+            g = Graphics.FromImage(canvas);
+
             InitializeComponent();
+            this.DoubleBuffered = true;
+
+            g.Clear(Color.Black);
+            Invalidate();
         }
 
 
         List<string> codaAudio = new List<string>();
+        List<float> campioniCanzone = new List<float>();
 
         private void caricaFileAudio(object sender, EventArgs e)
         {
+            campioniCanzone.Clear();
             string audio = aggiungiAudioInCoda();
-            if (audio != null)
+            if (!String.IsNullOrEmpty(audio))
             {
                 var mp3Reader = new Mp3FileReader(audio);
+                toolStripStatusLabel_nomeCanzone.Text = audio;
 
-                // ISampleProvider fornisce campioni già come float normalizzati
+                //ISampleProvider fornisce campioni già come float normalizzati
                 ISampleProvider sampleProvider = mp3Reader.ToSampleProvider();
                 float[] samples = new float[4096];
                 int samplesRead;
                 int iterazione = 0;
+                float max = 0;
                 while ((samplesRead = sampleProvider.Read(samples, 0, samples.Length)) > 0)
                 {
                     for (int i = 0; i < samplesRead; i++)
                     {
-                        float sample = samples[i]; // valore tra -1.0f e +1.0f
-                                                   .
+                        // samples[i] conteine un valore tra -1.0f e +1.0f
+                       campioniCanzone.Add(samples[i]);
+                        if (samples[i] > max)
+                        {
+                            max = samples[i];
+                        }
                     }
                     iterazione++;
                 }
+                disegnaOnda();
+                Invalidate();
+            }
+        }
+
+        private void disegnaOnda()
+        {
+            g.Clear(Color.Black);
+            centerY = height_osc / 2;
+            g.DrawLine(utilityPen, 0, centerY, width_osc - 1, centerY);
+
+            int totaleCampioni = campioniCanzone.Count;
+
+            for (int x = 0; x < width_osc - 1; x++)
+            {
+                //idxStart : totaleCampioni = x : width_osc
+                int idxStart = (int)(((float)x / width_osc) * totaleCampioni);
+                int idxEnd = (int)(((float)(x + 1) / width_osc) * totaleCampioni);
+                idxEnd = Math.Min(idxEnd, totaleCampioni - 1);  //Serve per sicurezza per non andare fuori dalla lista
+
+                // Trova il valore massimo e minimo del blocco
+                float max = 0f, min = 0f;
+                for (int i = idxStart; i <= idxEnd; i++)
+                {
+                    if (campioniCanzone[i] > max)
+                    {
+                        max = campioniCanzone[i] * 1f;
+                    }
+                    if (campioniCanzone[i] < min)
+                    {
+                        min = campioniCanzone[i] * 1f;
+                    }
+                }
+
+                // Converti in coordinate Y
+                //Con (max + 1f) / 2f) normalizzo per ricondurmi all'intervallo [0, 1] dato che i float vanno da -1f a + 1f
+                //All'inizio metto 1f - ... in modo da invertire la forma d'onda dato che nel grafico y=0 sta in alto e non in basso
+                int yMax = (int)(((1f - (max + 1f) / 2f)) * height_osc);
+                int yMin = (int)(((1f - (min + 1f) / 2f)) * height_osc);
+
+                //Clamp di sicurezza per evitare che la forma d'onda esca dal grafico
+                yMax = Math.Max(0, Math.Min(yMax, height_osc - 1));
+                yMin = Math.Max(0, Math.Min(yMin, height_osc - 1));
+
+                // Disegna una linea verticale dal minimo al massimo
+                g.DrawLine(samplePen, x, yMin, x, yMax);
             }
         }
 
@@ -62,6 +135,39 @@ namespace GainAudioPlayer
             }
 
             return filePath;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            e.Graphics.FillRectangle(Brushes.Black, 10, 30, width_osc, height_osc);
+            e.Graphics.DrawRectangle(Pens.Red, 9, 29, width_osc + 1, height_osc + 1);
+            centerY = height_osc / 2;
+            g.DrawLine(utilityPen, 0, centerY, width_osc - 1, centerY);
+            e.Graphics.DrawImage(canvas, 10, 30);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            width_osc = this.Width - 40;
+            height_osc = this.Height - 100;
+
+            // Ricrea il bitmap con le nuove dimensioni
+            g?.Dispose();
+            canvas?.Dispose();
+            canvas = new Bitmap(width_osc, height_osc);
+            g = Graphics.FromImage(canvas);
+
+            //disegna sul bitmap aggiornato
+            g.Clear(Color.Black);
+            centerY = height_osc / 2;
+            g.DrawLine(utilityPen, 0, centerY, width_osc - 1, centerY);
+            if (campioniCanzone != null && campioniCanzone.Count > 0)
+            {
+                disegnaOnda();
+            }
+            Invalidate();
         }
     }
 }
